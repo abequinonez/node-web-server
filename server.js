8ced955805181958df7a6ada390981e7989a1791
abequinonez <abequinonez@users.noreply.github.com>
2018-03-12 19:46:35 -0700

    docs: Add comments to better explain code

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const app = express();
 const hbs = require('hbs');
 const fs = require('fs');
 
+// Use port 3000 if no environment variable is set
 const port = process.env.PORT || 3000;
 
+// Register a partials directory
 hbs.registerPartials(__dirname + '/views/partials');
+
+// Set Handlebars as the default view engine
 app.set('view engine', 'hbs');
 
+// Create and use a middleware function that logs server requests
 app.use((req, res, next) => {
   const now = new Date().toString();
   const log = `${now}: ${req.method} ${req.url}`;
@@ -21,16 +26,23 @@ app.use((req, res, next) => {
   next();
 });
 
+/*
+Create and use a middleware function that renders a maintenance page and
+prevents any of the HTTP handlers below from running.
+*/
 // app.use((req, res, next) => {
 //   res.render('maintenance.hbs');
 // });
 
+// Use a built-in middleware that sets up a static directory
 app.use(express.static(__dirname + '/public'));
 
+// Register a helper function that returns the current year
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
 });
 
+// Register a helper function that converts a passed-in string to uppercase
 hbs.registerHelper('screamIt', str => {
   return str.toUpperCase() + '!';
 });
